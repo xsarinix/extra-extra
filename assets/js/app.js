3867f986e0ac9cab8bd0ac41dd98d780b04fd389
Sarah Grabinski <sarah.grabinski@gmail.com>
2019-02-15 17:27:38 -0500

    Chart built with axes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,28 @@
-// // Define SVG area dimensions
-// var svgWidth = 960;
-// var svgHeight = 660;
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
 
-// // Define the chart's margins as an object
-// var chartMargin = {
-//   top: 30,
-//   right: 30,
-//   bottom: 30,
-//   left: 30
-// };
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30
+};
 
-// // Define dimensions of the chart area
-// var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-// var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
 
-// // Select body, append SVG area to it, and set the dimensions
-// var svg = d3.select("body")
-//   .append("svg")
-//   .attr("height", svgHeight)
-//   .attr("width", svgWidth);
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
 
-// // Append a group to the SVG area and shift ('translate') it to the right and to the bottom
-// var chartGroup = svg.append("g")
-//   .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+// Append a group to the SVG area and shift ('translate') it to the right and to the bottom
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
 // Load data from hours-of-tv-watched.csv
 d3.csv("assets/data/data.csv").then(function(censusData, error) {
@@ -50,30 +50,30 @@ d3.csv("assets/data/data.csv").then(function(censusData, error) {
 
   console.log(censusData);
 
-//   // Configure a band scale for the horizontal axis with a padding of 0.1 (10%)
-//   var xBandScale = d3.scaleBand()
-//     .domain(censusData.map(d => d.name))
-//     .range([0, chartWidth])
-//     .padding(0.1);
+  // Configure a band scale for the horizontal axis with a padding of 0.1 (10%)
+  var xBandScale = d3.scaleBand()
+    .domain(censusData.map(d => d.poverty))
+    .range([0, chartWidth])
+    .padding(0.1);
 
-//   // Create a linear scale for the vertical axis.
-//   var yLinearScale = d3.scaleLinear()
-//     .domain([0, d3.max(censusData, d => d.hours)])
-//     .range([chartHeight, 0]);
+  // Create a linear scale for the vertical axis.
+  var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(censusData, d => d.obesity)])
+    .range([chartHeight, 0]);
 
-//   // Create two new functions passing our scales in as arguments
-//   // These will be used to create the chart's axes
-//   var bottomAxis = d3.axisBottom(xBandScale);
-//   var leftAxis = d3.axisLeft(yLinearScale).ticks(10);
+  // Create two new functions passing our scales in as arguments
+  // These will be used to create the chart's axes
+  var bottomAxis = d3.axisBottom(xBandScale);
+  var leftAxis = d3.axisLeft(yLinearScale).ticks(10);
 
-//   // Append two SVG group elements to the chartGroup area,
-//   // and create the bottom and left axes inside of them
-//   chartGroup.append("g")
-//     .call(leftAxis);
+  // Append two SVG group elements to the chartGroup area,
+  // and create the bottom and left axes inside of them
+  chartGroup.append("g")
+    .call(leftAxis);
 
-//   chartGroup.append("g")
-//     .attr("transform", `translate(0, ${chartHeight})`)
-//     .call(bottomAxis);
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${chartHeight})`)
+    .call(bottomAxis);
 
 //   // Create one SVG rectangle per piece of censusData
 //   // Use the linear and band scales to position each rectangle within the chart
