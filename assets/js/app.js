8667a31833c5af7c98a3acfa10958acd159c57e7
Sarah Grabinski <sarah.grabinski@gmail.com>
2019-02-15 18:38:42 -0500

    Chart done with scaling

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,10 +4,10 @@ var svgHeight = 660;
 
 // Define the chart's margins as an object
 var chartMargin = {
-  top: 30,
-  right: 30,
-  bottom: 30,
-  left: 30
+  top: 40,
+  right: 40,
+  bottom: 40,
+  left: 40
 };
 
 // Define dimensions of the chart area
@@ -51,20 +51,48 @@ d3.csv("assets/data/data.csv").then(function(censusData, error) {
   console.log(censusData);
 
   // Configure a band scale for the horizontal axis with a padding of 0.1 (10%)
-  var xBandScale = d3.scaleBand()
-    .domain(censusData.map(d => d.poverty))
-    .range([0, chartWidth])
-    .padding(0.1);
+  var xLinearScale = d3.scaleLinear()
+    .domain([5, d3.max(censusData, d => d.poverty)+5])
+    .range([0, chartWidth]);
 
   // Create a linear scale for the vertical axis.
   var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(censusData, d => d.obesity)])
+    .domain([0, d3.max(censusData, d => d.obesity)+10])
     .range([chartHeight, 0]);
 
   // Create two new functions passing our scales in as arguments
   // These will be used to create the chart's axes
-  var bottomAxis = d3.axisBottom(xBandScale);
-  var leftAxis = d3.axisLeft(yLinearScale).ticks(10);
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // // gridlines in x axis function
+  // function make_x_gridlines() {		
+  //   return d3.axisBottom(xLinearScale)
+  //       .ticks(5)
+  // }
+
+  // // gridlines in y axis function
+  // function make_y_gridlines() {		
+  //   return d3.axisLeft(yLinearScale)
+  //       .ticks(5)
+  // }
+
+  // // add the X gridlines
+  // svg.append("g")			
+  //   .attr("class", "grid")
+  //   .attr("transform", "translate(0," + chartHeight + ")")
+  //   .call(make_x_gridlines()
+  //       .tickSize(-chartHeight)
+  //       .tickFormat("")
+  //   )
+
+  // // add the Y gridlines
+  // svg.append("g")			
+  //   .attr("class", "grid")
+  //   .call(make_y_gridlines()
+  //       .tickSize(-chartWidth)
+  //       .tickFormat("")
+  //   )
 
   // Append two SVG group elements to the chartGroup area,
   // and create the bottom and left axes inside of them
@@ -75,16 +103,28 @@ d3.csv("assets/data/data.csv").then(function(censusData, error) {
     .attr("transform", `translate(0, ${chartHeight})`)
     .call(bottomAxis);
 
-//   // Create one SVG rectangle per piece of censusData
-//   // Use the linear and band scales to position each rectangle within the chart
-//   chartGroup.selectAll(".bar")
-//     .data(censusData)
-//     .enter()
-//     .append("rect")
-//     .attr("class", "bar")
-//     .attr("x", d => xBandScale(d.name))
-//     .attr("y", d => yLinearScale(d.hours))
-//     .attr("width", xBandScale.bandwidth())
-//     .attr("height", d => chartHeight - yLinearScale(d.hours));
+  // Create one SVG rectangle per piece of censusData
+  // Use the linear and band scales to position each rectangle within the chart
+  chartGroup.selectAll("#scatter")
+    .data(censusData)
+    .enter()
+    .append("circle")
+    .attr("class", "scatter")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.obesity))
+    .attr("r", 5)
+    .attr("fill", "blue");
 
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - chartMargin.left)
+    .attr("x", 0 - (chartHeight / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("Obesity Rate");
+
+  chartGroup.append("text")
+    .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top-5})`)
+    .attr("class", "axisText")
+    .text("Poverty Rate");
 });
